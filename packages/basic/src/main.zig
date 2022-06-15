576e06eddcadadafef602743cf943f7984f2c286
hhstore <hhglory@outlook.com>
2022-06-15 15:36:34 +0800

    update

--- packages/basic/src/main.zig
+++ packages/basic/src/main.zig
@@ -2,8 +2,18 @@ const std = @import("std");
 
 pub fn main() anyerror!void {
     std.log.info("All your codebase are belong to us.", .{});
+    std.debug.print("Hello, {s}!\n", .{"World"});
 }
 
 test "basic test" {
     try std.testing.expectEqual(10, 3 + 7);
+
+    std.log.debug("All your codebase are belong to us.", .{});
+    // std.log.err("All your codebase are belong to us.", .{});
+    std.log.info("All your codebase are belong to us.", .{});
+}
+
+test "test2" {
+    try std.testing.expectEqual(10, 3 + 7);
+    try std.testing.expectEqualStrings("Hello, World!", "Hello, World!");
 }
