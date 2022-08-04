49104865cde3aaf796d768fe928b2d4ed5d81a80
Henry.Huang <hhglory@outlook.com>
2022-08-04 19:47:24 +0800

    update: c call zig

--- packages/zig-to-c/src/mathtest.zig
+++ packages/zig-to-c/src/mathtest.zig
@@ -1,4 +1,8 @@
+const print = @import("std").debug.print;
+
 //
 pub export fn add(a: i32, b: i32) i32 {
+    // todo x: print args
+    print("zig log: add({}, {})\n", .{ a, b });
     return a + b;
 }
